7a1da28ad19c35a190666259533ee9766ec553f3
Oded Lachish <oded@dcs.bbk.ac.uk>
2012-02-23 16:33:39 +0000

    Mockito answered the second method call correctly

--- Mocking/src/tests/CounterTest.java
+++ Mocking/src/tests/CounterTest.java
@@ -24,7 +24,12 @@ public class CounterTest {
 		Counter count = new Counter();
 		
 		Counter mockedCounter = mock(Counter.class);
-		when(mockedCounter.getValue()).thenReturn(1);
+		when(mockedCounter.getValue()).thenReturn(1).thenReturn(2);
+		
+		first = count.getValue();
+		mockedFirst = mockedCounter.getValue();
+		
+		assertEquals("Wronge Answer !",first , mockedFirst);
 		
 		first = count.getValue();
 		mockedFirst = mockedCounter.getValue();
